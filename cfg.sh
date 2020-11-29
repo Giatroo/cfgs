c2cd0b3406676899dbf6c930db4ac9843541854c
Lucas Paiolla <lucaspaiolla@gmail.com>
2020-11-29 17:49:31 -0300

    Update cfg script

--- cfg.sh
+++ cfg.sh
@@ -15,7 +15,7 @@ cp -r ~/.config/user/zsh-syntax-highlighting ~/
 cp -r ~/.config/user/nvim ~/.config/
 
 echo Installing main programs
-sudo apt install arc-theme build-essential gnome-tweaks git curl nodejs
+sudo apt install arc-theme build-essential gnome-tweaks git curl npm nodejs
 
 echo Installing terminal programs
 sudo apt install zsh neovim tmux vim-gtk3 fonts-powerline
@@ -25,9 +25,17 @@ sudo apt install gcc g++ clang clang-format python3 octave valgrind latexmk waka
 
 echo Installing python things
 sudo apt install python3-pip
+pip install jupyter-kite
+jupyter labextension install "@kiteco/jupyterlab-kite"
 
 echo Installing zathura
 sudo apt-get install zathura zathura-cb zathura-ps zathura-djvu
+
+echo Installing miktex
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys D6BC243565B2087BC3F897C9277A7293F59E4889
+echo "deb http://miktex.org/download/ubuntu focal universe" | sudo tee /etc/apt/sources.list.d/miktex.list
+sudo apt-get update
+sudo apt-get install miktex
 
 echo Installing onedark theme for terminal
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/denysdovhan/gnome-terminal-one/master/one-dark.sh)"
@@ -43,6 +51,9 @@ echo Installing oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 #chsh -s $(which zsh)
 
+echo Installing Kite
+bash -c "$(wget -q -O - https://linux.kite.com/dls/linux/current)"
+
 echo Installing wakatime
 pip install wakatime
 git clone https://github.com/sobolevn/wakatime-zsh-plugin.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/wakatime
@@ -56,6 +67,7 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 echo Installing tmux plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
+echo Installing python nvim support
 pip3 install pynvim
 
 popd

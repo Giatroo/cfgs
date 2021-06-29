12338b6bb7a6454896fc2bfc557818afedce02a7
Lucas Paiolla <lucaspaiolla@gmail.com>
2021-06-29 08:19:33 -0300

    configuring my polybar rofi scripts.

--- polybar/giatro/scripts/launcher.sh
+++ polybar/giatro/scripts/launcher.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-FILE="$HOME/.config/polybar/forest/scripts/rofi/colors.rasi"
+FILE="$HOME/.config/polybar/giatro/scripts/rofi/colors.rasi"
 
 ## random accent color
 #COLORS=('#EC7875' '#EC6798' '#BE78D1' '#75A4CD' '#00C7DF' '#00B19F' '#61C766' \
@@ -10,4 +10,13 @@ FILE="$HOME/.config/polybar/forest/scripts/rofi/colors.rasi"
 #sed -i -e "s/ac: .*/ac:   ${AC}FF;/g" $FILE
 #sed -i -e "s/se: .*/se:   ${SE}FF;/g" $FILE
 
-rofi -no-config -no-lazy-grab -show drun -modi drun -theme ~/.config/polybar/forest/scripts/rofi/launcher.rasi
+# rofi -no-config -no-lazy-grab -show drun -modi drun -theme ~/.config/polybar/giatro/scripts/rofi/launcher.rasi
+
+theme="kde_krunner"
+dir="$HOME/.config/rofi/launchers/misc"
+
+# comment these lines to disable random style
+# themes=($(ls -p --hide="launcher.sh" $dir))
+# theme="${themes[$(( $RANDOM % 16 ))]}"
+
+rofi -no-lazy-grab -show drun -modi drun,window,run -theme $dir/"$theme"

c9dce34dbcd14087f13f2ba6e4e5bcd4c2105d95
Lucas Paiolla <lucaspaiolla@gmail.com>
2024-11-07 15:46:58 -0300

    feat(nvim): create trouble which-key group

--- nvim/lua/plugins/trouble.lua
+++ nvim/lua/plugins/trouble.lua
@@ -1,43 +1,48 @@
 return {
-  "folke/trouble.nvim",
-  opts = {
-    use_diagnostics_signs = true,
-    action_keys = {
-        close = { "q", "<esc>" },
-        cancel = "<c-e>",
-    }
-  },
-  cmd = "Trouble",
-  keys = {
-    {
-      "<leader>xx",
-      "<cmd>Trouble diagnostics toggle<cr>",
-      desc = "Diagnostics (Trouble)",
-    },
-    {
-      "<leader>xX",
-      "<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
-      desc = "Buffer Diagnostics (Trouble)",
-    },
-    {
-      "<leader>cs",
-      "<cmd>Trouble symbols toggle focus=false<cr>",
-      desc = "Symbols (Trouble)",
-    },
-    {
-      "<leader>cl",
-      "<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
-      desc = "LSP Definitions / references / ... (Trouble)",
-    },
-    {
-      "<leader>xL",
-      "<cmd>Trouble loclist toggle<cr>",
-      desc = "Location List (Trouble)",
-    },
-    {
-      "<leader>xQ",
-      "<cmd>Trouble qflist toggle<cr>",
-      desc = "Quickfix List (Trouble)",
-    },
-  },
+	"folke/trouble.nvim",
+	opts = {
+		use_diagnostics_signs = true,
+		action_keys = {
+			close = { "q", "<esc>" },
+			cancel = "<c-e>",
+		},
+	},
+	cmd = "Trouble",
+	keys = {
+		{
+			"<leader>xx",
+			"<cmd>Trouble diagnostics toggle<cr>",
+			desc = "Diagnostics",
+		},
+		{
+			"<leader>xX",
+			"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+			desc = "Buffer Diagnostics",
+		},
+		{
+			"<leader>xs",
+			"<cmd>Trouble symbols toggle focus=false<cr>",
+			desc = "Symbols",
+		},
+		{
+			"<leader>xl",
+			"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+			desc = "LSP Definitions / references / ...",
+		},
+		{
+			"<leader>xL",
+			"<cmd>Trouble loclist toggle<cr>",
+			desc = "Location List",
+		},
+		{
+			"<leader>xQ",
+			"<cmd>Trouble qflist toggle<cr>",
+			desc = "Quickfix List",
+		},
+	},
+	config = function()
+		require("which-key").add({
+			{ "<leader>x", group = "Trouble" },
+		})
+	end,
 }
